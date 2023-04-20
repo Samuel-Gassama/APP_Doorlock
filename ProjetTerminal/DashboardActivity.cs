4f9b82ccb4c8927e1438666307491f2bb4c1cce2
Samuel-Gassama <samgassa@hotmail.fr>
2023-04-20 15:25:49 -0400

    Ajout du login fonctionnel

---
+++ ProjetTerminal/DashboardActivity.cs
@@ -0,0 +1,25 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Widget;
+using AndroidX.AppCompat.App;
+
+namespace ProjetTerminal
+{
+    [Activity(Label = "DashboardActivity")]
+    public class DashboardActivity : AppCompatActivity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            SetContentView(Resource.Layout.dashboard);
+
+            // Get the user information from the intent extras
+            string userId = Intent.GetStringExtra("user_id");
+            string email = Intent.GetStringExtra("email");
+
+            // Display the user information on the screen
+            TextView textView = FindViewById<TextView>(Resource.Id.user_info);
+            textView.Text = $"User ID: {userId}, Email: {email}";
+        }
+    }
+}
--- ProjetTerminal/MainActivity.cs
+++ ProjetTerminal/MainActivity.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Net.Http;
+using Android.Content;
+using System.Text;
 using Android.App;
 using Android.OS;
 using Android.Runtime;
@@ -7,6 +10,10 @@ using AndroidX.AppCompat.Widget;
 using AndroidX.AppCompat.App;
 using Google.Android.Material.FloatingActionButton;
 using Google.Android.Material.Snackbar;
+using Android.Widget;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace ProjetTerminal
 {
@@ -18,30 +25,16 @@ namespace ProjetTerminal
             base.OnCreate(savedInstanceState);
             SetContentView(Resource.Layout.login);
 
-        }
+            Button loginButton = FindViewById<Button>(Resource.Id.loginButton);
+            EditText emailEditText = FindViewById<EditText>(Resource.Id.emailEditText);
+            EditText passwordEditText = FindViewById<EditText>(Resource.Id.passwordEditText);
 
-        public override bool OnCreateOptionsMenu(IMenu menu)
-        {
-            MenuInflater.Inflate(Resource.Menu.menu_main, menu);
-            return true;
-        }
-
-        public override bool OnOptionsItemSelected(IMenuItem item)
-        {
-            int id = item.ItemId;
-            if (id == Resource.Id.action_settings)
+            loginButton.Click += async (sender, e) =>
             {
-                return true;
-            }
-
-            return base.OnOptionsItemSelected(item);
-        }
-
-        private void FabOnClick(object sender, EventArgs eventArgs)
-        {
-            View view = (View) sender;
-            Snackbar.Make(view, "Replace with your own action", Snackbar.LengthLong)
-                .SetAction("Action", (View.IOnClickListener)null).Show();
+                string email = emailEditText.Text;
+                string password = passwordEditText.Text;
+                await LoginUser(email, password);
+            };
         }
 
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
@@ -51,8 +44,39 @@ namespace ProjetTerminal
             base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
         }
 
+        private async Task LoginUser(string email, string password)
+        {
+            try
+            {
+                using (var httpClient = new HttpClient())
+                {
+                    var requestContent = new StringContent(JsonConvert.SerializeObject(new { email, password }), Encoding.UTF8, "application/json");
+                    var response = await httpClient.PostAsync("http://10.0.2.2:8000/api/login", requestContent);
+                    if (response.IsSuccessStatusCode)
+                    {
+                        string responseBody = await response.Content.ReadAsStringAsync();
+                        var result = JsonConvert.DeserializeObject<Dictionary<string, string>>(responseBody);
 
+                        var intent = new Intent(this, typeof(DashboardActivity));
+                        intent.PutExtra("user_id", result["user_id"]);
+                        intent.PutExtra("email", result["email"]);
+                        StartActivity(intent);
+                    }
+                    else
+                    {
+                        RunOnUiThread(() =>
+                        {
+                            Toast.MakeText(this, "Invalid email or password", ToastLength.Short).Show();
+                        });
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Exception: " + ex.Message);
+                Console.WriteLine("Stack Trace: " + ex.StackTrace);
+            }
+        }
 
-	}
+    }
 }
-
