ec49bdff75558feb3dc7c3f4807e3d7dddfd905c
Samuel-Gassama <samgassa@hotmail.fr>
2023-05-11 10:12:02 -0400

    Ajout des commentaires

--- ProjetTerminal/MQTTInfoActivity.cs
+++ ProjetTerminal/MQTTInfoActivity.cs
@@ -1,11 +1,10 @@
 ﻿/**
- * Receiving the MQTT Data info 
+ * Réception des informations MQTT
  * @file MQTTInfoActivity.cs
  * @author 
  *     - Samuel Gassama
  * @version 1.5 : 28/04/2023
  */
-
 
 using System;
 using System.Linq;
@@ -32,22 +31,19 @@ using System.Collections.Generic;
 
 namespace ProjetTerminal
 {
-    [Activity(Label = "MQTTInfoActivity", Theme = "@style/AppTheme.NoActionBar", MainLauncher = false)]
-
+    [Activity(Label = "MQTTInfoActivity")]
     public class MQTTInfoActivity : Activity
     {
         IManagedMqttClient client;
         private List<IDictionary<string, object>> scannedKeys = new List<IDictionary<string, object>>();
-        private string currentLanguage;
 
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
+
             SetContentView(Resource.Layout.listeEntrees);
 
-            currentLanguage = Java.Util.Locale.Default.Language;
-
-            // Configure the MQTT client
+            // Configuration du client MQTT
             var options = new MqttClientOptionsBuilder()
                 .WithTcpServer("172.16.5.100", 1883)
                 .WithCredentials("mams", "mams")
@@ -56,74 +52,48 @@ namespace ProjetTerminal
             client = new MqttFactory().CreateManagedMqttClient();
             client.ConnectedHandler = new MqttClientConnectedHandlerDelegate(args =>
             {
-                Console.WriteLine("Connected to MQTT broker");
+                Console.WriteLine("Connecté au courtier MQTT");
                 client.SubscribeAsync(new MqttTopicFilterBuilder().WithTopic("door/lock").Build());
             });
             client.DisconnectedHandler = new MqttClientDisconnectedHandlerDelegate(args =>
             {
-                Console.WriteLine("Disconnected from MQTT broker: " + args.Exception.Message);
+                Console.WriteLine("Déconnecté du courtier MQTT : " + args.Exception.Message);
             });
             client.ApplicationMessageReceivedHandler = new MqttApplicationMessageReceivedHandlerDelegate(args =>
             {
+                // Réception d'un message MQTT
                 string payload = Encoding.UTF8.GetString(args.ApplicationMessage.Payload);
                 JObject jObject = JObject.Parse(payload);
                 string cardId = jObject["cardId"].ToString();
                 string name = jObject["name"].ToString();
                 string door = jObject["door"].ToString();
 
-                // Add the scanned key to the list
+                // Ajouter la clé scannée à la liste
                 var scannedKey = new JavaDictionary<string, object> {
-                {"ID", cardId},
-                {"Statut", name},
-                {"Porte", door},
-                {"Date et Heure", DateTime.Now.ToString()}
-            };
+                    {"ID", cardId},
+                    {"Statut", name},
+                    {"Porte", door},
+                    {"Date et Heure", DateTime.Now.ToString()}
+                };
                 scannedKeys.Add(scannedKey);
 
-                // Update the list view with all the scanned keys
+                // Mettre à jour la liste des clés scannées dans la vue
                 RunOnUiThread(() =>
                 {
-                    var adapter = new SimpleAdapter(this, scannedKeys, Resource.Layout.mqtt_list_item, new[] { "ID", "Statut", "Porte", "Date et Heure" }, new[] {
-        Resource.Id.textView1, Resource.Id.textView2, Resource.Id.textView3, Resource.Id.textView4
-    });
+                    var adapter = new SimpleAdapter(this, scannedKeys, Resource.Layout.listeEntrees, new[] { "ID", "Statut", "Porte", "Date et Heure" }, new[] {
+                        Resource.Id.textView1, Resource.Id.textView2, Resource.Id.textView3, Resource.Id.textView4
+                    });
                     ListView mqttInfoListView = FindViewById<ListView>(Resource.Id.mqttInfoListView);
                     mqttInfoListView.Adapter = adapter;
                 });
-
             });
 
-
-
-            // Add a button to go to the SettingsActivity
-            Button settingsButton = FindViewById<Button>(Resource.Id.settingsButton);
-            settingsButton.Click += (sender, args) =>
-            {
-                var intent = new Intent(this, typeof(SettingsActivity));
-                StartActivity(intent);
-            };
-
-            // Connect to the MQTT broker
+            // Se connecter au courtier MQTT
             client.StartAsync(
                 new ManagedMqttClientOptionsBuilder()
                     .WithAutoReconnectDelay(TimeSpan.FromSeconds(5))
                     .WithClientOptions(options)
                     .Build());
-
         }
-
-        protected override void OnResume()
-        {
-            base.OnResume();
-
-            if (!Java.Util.Locale.Default.Language.Equals(currentLanguage))
-            {
-                Recreate();
-            }
-
-
-        }
-
-
-
     }
 }
--- ProjetTerminal/SettingsActivity.cs
+++ ProjetTerminal/SettingsActivity.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Reflection.Emit;
 using Android.App;
 using Android.Content;
 using Android.OS;
@@ -20,24 +21,32 @@ namespace ProjetTerminal
             currentLanguage = Java.Util.Locale.Default.Language;
 
             languageButton = FindViewById<Button>(Resource.Id.languageButton);
+
             languageButton.Click += (sender, args) =>
             {
                 ToggleAppLanguage();
             };
         }
 
+        // Méthode pour basculer la langue de l'application
         private void ToggleAppLanguage()
         {
+            // Déterminer la nouvelle langue en fonction de la langue actuelle
             string newLanguage = currentLanguage == "en" ? "fr" : "en";
+
             SetAppLanguage(newLanguage);
             currentLanguage = newLanguage;
         }
 
+        // Méthode pour définir la langue de l'application
         private void SetAppLanguage(string languageCode)
         {
             var locale = new Java.Util.Locale(languageCode);
+
+            // Définir la langue par défaut de l'application sur la nouvelle locale
             Java.Util.Locale.Default = locale;
 
+            // Créer une nouvelle configuration avec la nouvelle locale
             var config = new Android.Content.Res.Configuration { Locale = locale };
             BaseContext.Resources.UpdateConfiguration(config, BaseContext.Resources.DisplayMetrics);
             Recreate();
@@ -47,6 +56,7 @@ namespace ProjetTerminal
         {
             base.OnResume();
 
+            // Vérifier si la langue par défaut a été modifiée
             if (!Java.Util.Locale.Default.Language.Equals(currentLanguage))
             {
                 Recreate();
