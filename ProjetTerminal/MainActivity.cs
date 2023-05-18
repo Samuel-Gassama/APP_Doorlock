ff4b2d1fa6ccf873159925c0178715fecaaeaec5
Samuel <81109272+Samuel-Gassama@users.noreply.github.com>
2023-05-18 17:50:13 -0400

    Update MainActivity.cs

--- ProjetTerminal/MainActivity.cs
+++ ProjetTerminal/MainActivity.cs
@@ -1,13 +1,3 @@
-﻿/**
- * Login Page 
- * @file MQTTInfoActivity.cs
- * @author 
- *     - Samuel Gassama
- * @version 1.6 : 12/05/2023
- */
-
-
-
 using System;
 using System.Net.Http;
 using Android.Content;
@@ -40,47 +30,61 @@ namespace ProjetTerminal
 
             currentLanguage = Java.Util.Locale.Default.Language;
 
-            //Button loginButton = FindViewById<Button>(Resource.Id.loginButton);
+            // Récupérer les références des éléments de l'interface utilisateur
             EditText emailEditText = FindViewById<EditText>(Resource.Id.emailEditText);
             EditText passwordEditText = FindViewById<EditText>(Resource.Id.passwordEditText);
 
+            // Définir l'événement de clic pour le bouton de connexion
             Button loginButton = FindViewById<Button>(Resource.Id.loginButton);
             loginButton.Click += (sender, e) =>
             {
+                // Créer une intention pour démarrer l'activité MQTTInfoActivity
                 var intent = new Intent(this, typeof(MQTTInfoActivity));
+
+                // Ajouter la langue actuelle en extra pour MQTTInfoActivity
                 intent.PutExtra("CurrentLanguage", currentLanguage);
+
+                // Démarrer l'activité MQTTInfoActivity
                 StartActivity(intent);
             };
 
-            // Add a button to go to the SettingsActivity
+            // Ajouter un bouton pour accéder à SettingsActivity
             languageButton = FindViewById<Button>(Resource.Id.languageButton);
             languageButton.Click += (sender, args) =>
             {
+                // Appeler la méthode ToggleAppLanguage lorsque le bouton de langue est cliqué
                 ToggleAppLanguage();
             };
-
-
         }
 
+        // Méthode pour basculer la langue de l'application
         private void ToggleAppLanguage()
         {
+            // Déterminer la nouvelle langue en fonction de la langue actuelle
             string newLanguage = currentLanguage == "en" ? "fr" : "en";
+
             SetAppLanguage(newLanguage);
+
             currentLanguage = newLanguage;
         }
 
-        // Méthode pour définir la langue de l'application
-
         private void SetAppLanguage(string languageCode)
         {
+            // Créer un nouvel objet Locale avec le code de langue spécifié
             var locale = new Java.Util.Locale(languageCode);
+
             Java.Util.Locale.Default = locale;
 
             var config = new Android.Content.Res.Configuration { Locale = locale };
+
+            // Mettre à jour les ressources de l'application avec la nouvelle configuration
             BaseContext.Resources.UpdateConfiguration(config, BaseContext.Resources.DisplayMetrics);
+
+            // Recréer l'activité pour appliquer le changement de langue
             Recreate();
         }
 
+        // Remplacer la méthode OnRequestPermissionsResult pour gérer les demandes d'autorisation
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
         {
             Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
@@ -88,18 +92,20 @@ namespace ProjetTerminal
             base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
         }
 
+        // Remplacer la méthode OnResume pour vérifier si la langue par défaut a été modifiée
         protected override void OnResume()
         {
             base.OnResume();
 
+            // Vérifier si la langue par défaut a été modifiée
             if (!Java.Util.Locale.Default.Language.Equals(currentLanguage))
             {
+                // Recréer l'activité pour appliquer le changement de langue
                 Recreate();
             }
         }
 
-
-
+        // Méthode pour gérer la connexion de l'utilisateur
         private async Task LoginUser(string email, string password)
         {
             try
@@ -108,18 +114,25 @@ namespace ProjetTerminal
                 {
                     var requestContent = new StringContent(JsonConvert.SerializeObject(new { email, password }), Encoding.UTF8, "application/json");
                     var response = await httpClient.PostAsync("http://10.0.2.2:8000/api/controller/login", requestContent);
+
                     if (response.IsSuccessStatusCode)
                     {
                         string responseBody = await response.Content.ReadAsStringAsync();
                         var result = JsonConvert.DeserializeObject<Dictionary<string, string>>(responseBody);
 
+                        // Créer une intention pour démarrer l'activité MQTTInfoActivity
                         var intent = new Intent(this, typeof(MQTTInfoActivity));
+
+                        // Ajouter les identifiants utilisateur en extra pour MQTTInfoActivity
                         intent.PutExtra("user_id", result["user_id"]);
                         intent.PutExtra("email", result["email"]);
+
+                        // Démarrer l'activité MQTTInfoActivity
                         StartActivity(intent);
                     }
                     else
                     {
+                        // Afficher un message toast en cas d'email ou de mot de passe non valide
                         RunOnUiThread(() =>
                         {
                             Toast.MakeText(this, "Invalid email or password", ToastLength.Short).Show();
@@ -129,6 +142,7 @@ namespace ProjetTerminal
             }
             catch (Exception ex)
             {
+                // Afficher les détails de l'exception dans la console
                 Console.WriteLine("Exception: " + ex.Message);
                 Console.WriteLine("Stack Trace: " + ex.StackTrace);
             }
